029fa830da16aaacf44b598b3feec9809d2d9819
r-aozora <zytrahidayat11@gmail.com>
2024-09-30 21:06:32 +0700

    first commit

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:dart_library/hello.dart';
+
+void main() {
+  print(sayHello('World'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+  print(customer);
+  print(product);
+  print(category); 
+}
